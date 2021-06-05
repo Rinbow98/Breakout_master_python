b91f0c069b4ae73b0ba4659e82499e4d3fd43bb5
daaaaaaavid <b0932296807@gmail.com>
2021-06-05 11:42:34 +0800

    Update brick.py

--- brick.py
+++ brick.py
@@ -4,18 +4,18 @@ class brick:
     
     def __init__( self, pg, screen, n):
         self.id = n   
-        self.color = (255, 0, 0)
         self.pg = pg
         self.screen = screen
-        self.i = int( (self.id) / 11 )
-        self.j = int( (self.id) % 11 )
-        
-    def draw(self):
-        
-        if position.level1[self.i][self.j] == 1:
-            self.pg.draw.rect(self.screen, self.color, (30*self.j, 10*self.i, 30, 10), 2)
-        self.i += 1
-        self.j += 1
+        self.i = int( (self.id) / position.width )
+        self.j = int( (self.id) % position.width)
+        self.windowWidth = 1280
+        self.windowHeight = 720
+        self.brk_image = self.pg.image.load("C:\\Users\\david\\Desktop\\python\\期末專題\\GitHub\\Breakout_master_python\\images\\redBrick5.png")
+    def draw(self):       
+        self.brk_image.convert()        
+        if position.level[self.i][self.j] == 1:
+          self.screen.blit(self.brk_image, (self.j*64,self.i*36+200))
+         
         
     def __str__(self):
         return str(self.id) + '\n'
