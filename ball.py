70c1d0a7d9175e4dc4ddb392c2effc9fdd3cbc5c
daaaaaaavid <b0932296807@gmail.com>
2021-06-06 23:30:32 +0800

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,24 @@
+import os,pygame
+class ball(pygame.sprite.Sprite):
+    def  __init__(self,windowWidth,windowHeight,x,y):
+       pygame.sprite.Sprite.__init__(self)
+       self.windowWidth = windowWidth
+       self.windowHight = windowHeight
+       self.radius = 20
+       self.image = pygame.image.load(os.path.join("images", "ball2.png")).convert_alpha()     
+       self.image = pygame.transform.smoothscale(self.image, (self.radius, self.radius))
+       self.rect = self.image.get_rect()
+       self.rect.x = x
+       self.rect.y = y - self.radius
+       self.state = 'onpad'
+       self.tempx = 0
+       self.tempy = 0
+    def update(self):
+       self.rect.x = self.tempx
+       self.rect.y = self.tempy
+       
+    def ball_onpad(self,x,y):
+        self.tempx = x - int(self.radius/2)
+        self.tempy = y - self.radius
+        
+    
