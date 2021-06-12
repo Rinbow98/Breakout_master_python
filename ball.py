a55c8a44deed9814386545799937cc12b5dfc220
daaaaaaavid <b0932296807@gmail.com>
2021-06-12 12:40:02 +0800

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,4 +1,4 @@
-import os, pygame
+import os, pygame 
 
 class ball(pygame.sprite.Sprite):
     def  __init__(self,windowWidth,windowHeight,x,y):
@@ -14,8 +14,8 @@ class ball(pygame.sprite.Sprite):
        self.state = 'onpad'
        self.tempx = 0
        self.tempy = 0
+       self.dir = 1
 
-       
     def update(self):
        self.rect.x = self.tempx
        self.rect.y = self.tempy
@@ -23,6 +23,63 @@ class ball(pygame.sprite.Sprite):
        
     def ball_onpad(self,x,y):
         self.tempx = x - self.radius // 2
-        self.tempy = y - self.radius
+        self.tempy = y - self.radius 
         
+    def move(self,speed):
+        
+        if self.dir==0:
+            self.tempy -= speed
+        elif self.dir==1:
+            self.tempy -= speed//2
+            self.tempx += speed//2
+        elif self.dir==2:            
+            self.tempx += speed
+        elif self.dir==3:
+            self.tempy += speed//2
+            self.tempx += speed//2
+        elif self.dir==4:
+            self.tempy += speed
+        elif self.dir==5:
+            self.tempy += speed//2
+            self.tempx -= speed//2
+        elif self.dir==6:
+            self.tempx -= speed
+        elif self.dir==7:
+            self.tempy -= speed//2
+            self.tempx -= speed//2
+      
+    def collide(self,things):
+        if things == "left":
+            if self.dir == 7:
+                self.dir = 1
+            elif self.dir == 6:
+                self.dir = 2
+            elif self.dir == 5:
+                self.dir = 3
+        elif things == "right":
+            if self.dir == 1:
+                self.dir = 7
+            elif self.dir == 2:
+                self.dir = 6
+            elif self.dir == 3:
+                self.dir = 5
+        elif things == "up":
+            if self.dir == 1:
+                self.dir = 3
+            elif self.dir == 0:
+                self.dir = 4
+            elif self.dir == 7:
+                self.dir = 5
+        elif things == "down":
+            if self.dir == 4:
+                self.dir = 0
+            elif self.dir == 3:
+                self.dir = 1
+            elif self.dir == 5:
+                self.dir = 7
+        
+      
+        
+    
+       
     
