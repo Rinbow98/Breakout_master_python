13d9c5d0937994c7dcc8d0ff127f25fd4e3e1957
daaaaaaavid <b0932296807@gmail.com>
2021-06-12 12:40:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ def main():
     pad = paddle.paddle(windowWidth, windowHeight)
     allsprite.add(pad)
     
-    ballx ,bally = pad.getxy()
-    gameball = ball.ball(windowWidth, windowWidth, ballx, bally)
+
+    gameball = ball.ball(windowWidth, windowWidth, pad.tempX, pad.rect.y)
     allsprite.add(gameball)
     
     score_board = scoreboard.scoreboard(windowWidth, windowHeight)
     allsprite.add(score_board)
     
     clock = pygame.time.Clock()
-    fps = 60
+    fps = 30
     speed = windowWidth // fps // 2
     mainLoop = True
     while mainLoop:
@@ -54,20 +54,54 @@ def main():
 
                 if event.key == pygame.K_F11:
                     pygame.display.toggle_fullscreen()
+                    
+                if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                    if ball_state == 'onpad':
+                        ball_state = 'moving'
+                    
                 
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == ord('a') :
                     pad.left = False
                 elif event.key == pygame.K_RIGHT or event.key == ord('d') :
                     pad.right = False
+                    
+        if ball_state == 'moving':            
+            gameball.move(speed)
+            if gameball.rect.x <= 0 :           
+                gameball.collide("left")
+                
+            elif gameball.rect.x >= windowWidth - 2*gameball.radius :
+                gameball.collide("right")
+                
+            elif gameball.rect.y <= windowHeight // 10:
+                gameball.collide("up")           
         
+            elif gameball.rect.y >= windowHeight - 2*gameball.radius:
+                mainLoop = False
+                
+            elif  gameball.rect.colliderect( pad ):
+                gameball.collide("down")
+
+            for i in range((position.height)*(position.width)):
+                if  gameball.rect.colliderect( brk[i].rect ) and allsprite.has(brk[i]):
+                    score_board.add(brk[i].point)
+                    allsprite.remove(brk[i])
+                    if gameball.rect.y >= brk[i].rect.y:
+                        gameball.collide("up")
+                    else:
+                        gameball.collide("down")
+
+                 #mainLoop = False
         pad.move(speed)
 
         if ball_state == 'onpad':
             x , y = pad.getxy()
             x +=  pad.paddleWidth // 2
             gameball.ball_onpad(x, y)
-        
+        elif ball_state == 'moving':
+            gameball.move(speed//2)
+
         screen.blit(background, (0, 0))
         
         for sprite in allsprite:
