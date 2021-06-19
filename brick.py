25cc48b92070b2245dce9f8f6bfd5dde9216a52f
daaaaaaavid <b0932296807@gmail.com>
2021-06-19 22:12:12 +0800

    Update brick.py

--- brick.py
+++ brick.py
@@ -3,7 +3,7 @@ import position
 
 class brick(pygame.sprite.Sprite):
     
-    def __init__( self, n, windowWidth, windowHeight):
+    def __init__( self, n, windowWidth, windowHeight , bonus):
         pygame.sprite.Sprite.__init__(self)
         self.id = n
         self.i = int( (self.id) / position.width )
@@ -16,8 +16,11 @@ class brick(pygame.sprite.Sprite):
         self.rect.x = self.j*self.brickWidth
         self.rect.y = (self.i+2)*self.brickHeight
         self.point = 10
-         
-        
+        self.bonus = bonus
+        self.state = 'stationary'
+        self.tempy = self.rect.y
+        self.bonusWidth = windowWidth//21
+        self.bonusHeight = windowHeight // 15
     def __str__(self):
         return str(self.id)
 
@@ -31,3 +34,30 @@ class brick(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.j*self.brickWidth
         self.rect.y = (self.i+2)*self.brickHeight
+    
+    def bonus_judge(self):
+        if self.bonus == 0:
+            pass
+        elif self.bonus == 1:
+             self.image_pic = pygame.image.load(os.path.join("images", "bonusPenetrate.png")).convert_alpha()
+             self.image = pygame.transform.smoothscale(self.image_pic, (self.bonusWidth, self.bonusHeight))
+             self.state = 'dropping'
+        elif self.bonus == 2:
+             self.image_pic = pygame.image.load(os.path.join("images", "bonusSticky.png")).convert_alpha()
+             self.image = pygame.transform.smoothscale(self.image_pic, (self.bonusWidth, self.bonusHeight))
+             self.state = 'dropping'
+        elif self.bonus == 3:
+             self.image_pic = pygame.image.load(os.path.join("images", "bonusWide.png")).convert_alpha()
+             self.image = pygame.transform.smoothscale(self.image_pic, (self.bonusWidth, self.bonusHeight))
+             self.state = 'dropping'
+        elif self.bonus == 4:
+             self.image_pic = pygame.image.load(os.path.join("images", "bonusWide.png")).convert_alpha()
+             self.image = pygame.transform.smoothscale(self.image_pic, (self.bonusWidth, self.bonusHeight))
+             self.state = 'dropping'
+             
+             
+    def move(self):
+        self.tempy += 5
+              
+    def update(self):
+        self.rect.y = self.tempy
